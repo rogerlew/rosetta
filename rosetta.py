877950160497340037f00cdfcc0caced44cfbf3c
Roger Lew <rogerlew@gmail.com>
2021-11-02 14:08:33 -0700

    Update rosetta.py

--- rosetta.py
+++ rosetta.py
@@ -28,9 +28,8 @@ from time import time
 from contextlib import closing
 from pprint import pprint
 
-from .ann import PTF_Model
-from .db import DB
-
+import ann
+import db
 
 class Rosetta(object):
     def __init__(self, model_no, debug=True):
@@ -52,11 +51,11 @@ class Rosetta(object):
         """
         self.debug = debug
 
-        with DB() as db:
+        with db.DB() as _db:
             if debug:
                 print("Getting models from database")
                 t0 = time()
-            self.ptf_model = PTF_Model(model_no, db)
+            self.ptf_model = ann.PTF_Model(model_no, _db)
 
             if debug:
                 print("Getting models from database, done (%s s)" % (time()-t0))
@@ -147,8 +146,8 @@ class Rosetta(object):
             print(sql_string)
             t0 = time()
 
-        with DB() as db:
-            with closing(db.get_cursor()) as cursor:
+        with db.DB() as _db:
+            with closing(_db.get_cursor()) as cursor:
                 cursor.execute(sql_string)
                 data = np.array(list(cursor))
                 data = data.transpose()
